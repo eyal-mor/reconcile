09583fc6afdf5e01386587318045aa47908fd8b3
Eyal Mor <eyalmor94@gmail.com>
2021-09-21 22:43:40 +0300

    Separate reconciler from observers.

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ mod operation;
 mod recurse;
 
 use operation::{Operation, OpType};
-use recurse::{recurse, Changes};
+use recurse::{recurse, Changes, Direction};
 
 pub fn debug_print<T1, T2> (v1: T1, v2: T2, p: &str) where T1: Debug, T2: Debug {
     println!("Old Value Is: {:?}", v1);
@@ -26,16 +26,39 @@ pub fn debug_print<T1, T2> (v1: T1, v2: T2, p: &str) where T1: Debug, T2: Debug 
     println!("Path is: {:?}", p);
 }
 
+trait Reconcile {
+    fn reconcile(&mut self) -> Changes;
+}
+
 pub struct Reconciler <'a> {
+    from: &'a SerdeValue,
+    to: &'a SerdeValue,
+}
+
+impl <'a> Reconciler <'a> {
+    pub fn new(from: &'a SerdeValue, to: &'a SerdeValue) -> Reconciler<'a> {
+        Reconciler{from, to}
+    }
+
+    pub fn reconcile(&mut self) -> Changes {
+        let mut changes = Changes::new();
+        recurse(&self.from, &self.to, "", &mut changes, Direction::OldToNew);
+        recurse(&self.to, &self.from, "", &mut changes, Direction::NewToOld);
+
+        return changes;
+    }
+}
+
+pub struct WorkerReconciler <'a> {
     from: &'a SerdeValue,
     to: &'a SerdeValue,
     observers: PathTree<Box<dyn worker::Worker<'a>>>,
     operations: HashMap<String, Operation>,
 }
 
-impl <'a> Reconciler <'a> {
-    pub fn new(from: &'a SerdeValue, to: &'a SerdeValue) -> Reconciler<'a> {
-        Reconciler{
+impl <'a> WorkerReconciler <'a> {
+    pub fn new(from: &'a SerdeValue, to: &'a SerdeValue) -> WorkerReconciler<'a> {
+        WorkerReconciler{
             from,
             to,
             observers: PathTree::new(),
@@ -49,9 +72,12 @@ impl <'a> Reconciler <'a> {
 
     pub fn reconcile(&mut self) {
         let mut changes = Changes::new();
-        recurse(&self.from, &self.to, "", &mut changes);
-        recurse(&self.to, &self.from, "", &mut changes);
+        recurse(&self.from, &self.to, "", &mut changes, Direction::OldToNew);
+        recurse(&self.to, &self.from, "", &mut changes, Direction::NewToOld);
+        self.call_observers_on_changes(changes);
+    }
 
+    fn call_observers_on_changes(&mut self, changes: Changes) {
         for (path, op) in changes.into_iter() {
             let path = path.as_str();
             match self.observers.find(path) {
@@ -118,10 +144,7 @@ mod tests {
         let mut reconciler = Reconciler::new(&from, &to);
         let worker = Box::from(WorkerMock{});
 
-        reconciler.add_observer("/arr/*/arr3/arrObj1", worker.clone());
-        reconciler.add_observer("/a", worker.clone());
         reconciler.reconcile();
-        // println!("Test update: {:#?}", changes);
     }
 
     #[test]
@@ -137,9 +160,22 @@ mod tests {
         let mut reconciler = Reconciler::new(&from, &to);
         let worker = Box::from(WorkerMock{});
 
-        reconciler.add_observer("/arr/*/arr3/arrObj1", worker.clone());
-        reconciler.add_observer("/a", worker.clone());
         reconciler.reconcile();
-        // println!("Test Delete: {:#?}", changes);
+    }
+
+    #[test]
+    fn test_create() {
+        let old_str = fs::read_to_string("./stubs/test-create/old.json").unwrap_or(String::from("{}"));
+        let old_str = old_str.as_str();
+        let new_str = fs::read_to_string("./stubs/test-create/new.json").unwrap_or(String::from("{}"));
+        let new_str = new_str.as_str();
+
+        let from: SerdeValue = serde_json::from_str(old_str).unwrap();
+        let to: SerdeValue = serde_json::from_str(new_str).unwrap();
+
+        let mut reconciler = Reconciler::new(&from, &to);
+        let worker = Box::from(WorkerMock{});
+
+        reconciler.reconcile();
     }
 }
